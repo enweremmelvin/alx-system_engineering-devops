8915ff64c287f7f7e8d68f02410f853db9fcb9a7
enweremmelvin <enweremmelvinconfidence@github.com>
2023-04-28 20:20:23 +0100

    Solution for task  -

--- 0x05-processes_and_signals/push_script.sh
+++ 0x05-processes_and_signals/push_script.sh
@@ -3,10 +3,10 @@
 
 commit_message=""
 
-read -p "Enter task number: " task_num
-read -p "Enter task title: " task_title
+read -rp "Enter task number: " task_num
+read -rp "Enter task title: " task_title
 
-commit_message=$(echo "Solution for task $task_num - $task_title")
+commit_message="Solution for task $task_num - $task_title"
 
 echo "Adding all files..."
 echo "-----------------------------------------------------"
